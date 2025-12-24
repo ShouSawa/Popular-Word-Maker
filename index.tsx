593a9525d4f1f7b6fe65f316a962b68dd04b5721
Shou <ShotaMain1211@gmail.com>
2025-12-25 00:29:42 +0900

    github pagesの設定の追加

--- index.tsx
+++ index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router basename="/Popular-Word-Maker">
+      <App />
+    </Router>
+  </React.StrictMode>,
 );
